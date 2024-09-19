732cb8f142a85da65cfcb2347243dcc1fb8c2150
fishmarch <zhang.xy@link.cuhk.edu.hk>
2024-09-19 13:31:15 +0800

    fix a bug of self_attn_mask

--- projects/mmdet3d_plugin/maptr/modules/__init__.py
+++ projects/mmdet3d_plugin/maptr/modules/__init__.py
@@ -1,5 +1,5 @@
 from .transformer import MapTRPerceptionTransformer
-from .decoder import MapTRDecoder, DecoupledDetrTransformerDecoderLayer, InstancePointAttention
+from .decoder import MapTRDecoder, DecoupledDetrTransformerDecoderLayer, InstancePointAttention, CustomTransformerLayer
 from .geometry_kernel_attention import GeometrySptialCrossAttention, GeometryKernelAttention
 from .height_kernel_attention import HeightKernelAttention, MSDeformableAttentionKernel
 from .builder import build_fuser
--- projects/mmdet3d_plugin/maptr/modules/decoder.py
+++ projects/mmdet3d_plugin/maptr/modules/decoder.py
@@ -375,6 +375,102 @@ class InstancePointAttention(BaseModule):
 
         return self.dropout(output) + identity
 
+
+@TRANSFORMER_LAYER.register_module()
+class CustomTransformerLayer(BaseTransformerLayer):
+
+    def __init__(self,
+                 attn_cfgs,
+                 feedforward_channels,
+                 ffn_dropout=0.0,
+                 operation_order=None,
+                 act_cfg=dict(type='ReLU', inplace=True),
+                 norm_cfg=dict(type='LN'),
+                 ffn_num_fcs=2,
+                 **kwargs):
+        super(CustomTransformerLayer, self).__init__(
+            attn_cfgs=attn_cfgs,
+            feedforward_channels=feedforward_channels,
+            ffn_dropout=ffn_dropout,
+            operation_order=operation_order,
+            act_cfg=act_cfg,
+            norm_cfg=norm_cfg,
+            ffn_num_fcs=ffn_num_fcs,
+            **kwargs)
+        assert len(operation_order) == 6
+        assert set(operation_order) == set(
+            ['self_attn', 'norm', 'cross_attn', 'ffn'])
+
+    def forward(self,
+                query,
+                key=None,
+                value=None,
+                query_pos=None,
+                key_pos=None,
+                attn_masks=None,
+                query_key_padding_mask=None,
+                key_padding_mask=None,
+                **kwargs):
+        norm_index = 0
+        attn_index = 0
+        ffn_index = 0
+        identity = query
+        if attn_masks is None:
+            attn_masks = [None for _ in range(self.num_attn)]
+        elif isinstance(attn_masks, torch.Tensor):
+            attn_masks = [
+                copy.deepcopy(attn_masks) for _ in range(self.num_attn)
+            ]
+            warnings.warn(f'Use same attn_mask in all attentions in '
+                          f'{self.__class__.__name__} ')
+        else:
+            assert len(attn_masks) == self.num_attn, f'The length of ' \
+                        f'attn_masks {len(attn_masks)} must be equal ' \
+                        f'to the number of attention in ' \
+                        f'operation_order {self.num_attn}'
+
+        for layer in self.operation_order:
+            if layer == 'self_attn':
+                temp_key = temp_value = query
+                query = self.attentions[attn_index](
+                    query,
+                    temp_key,
+                    temp_value,
+                    identity if self.pre_norm else None,
+                    query_pos=query_pos,
+                    key_pos=query_pos,
+                    attn_mask=kwargs['self_attn_mask'],
+                    key_padding_mask=query_key_padding_mask,
+                    **kwargs)
+                attn_index += 1
+                identity = query
+
+            elif layer == 'norm':
+                query = self.norms[norm_index](query)
+                norm_index += 1
+
+            elif layer == 'cross_attn':
+                query = self.attentions[attn_index](
+                    query,
+                    key,
+                    value,
+                    identity if self.pre_norm else None,
+                    query_pos=query_pos,
+                    key_pos=key_pos,
+                    attn_mask=attn_masks[attn_index],
+                    key_padding_mask=key_padding_mask,
+                    **kwargs)
+                attn_index += 1
+                identity = query
+
+            elif layer == 'ffn':
+                query = self.ffns[ffn_index](
+                    query, identity if self.pre_norm else None)
+                ffn_index += 1
+
+        return query
+
+
 @TRANSFORMER_LAYER.register_module()
 class DecoupledDetrTransformerDecoderLayer(BaseTransformerLayer):
     """Implements decoder layer in DETR transformer.
